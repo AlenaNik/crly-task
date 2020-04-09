c432f8b73616ae408cb2ffedc80a976a7a84283d
AlenaNik <alena.nik.digital@gmail.com>
2020-04-09 17:40:56 +0200

    port

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.put('/api/addresses/:id', (req, res) => {
   })
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
