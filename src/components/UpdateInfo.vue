e10dc3ef7d4d67343e829921143d455941857176
AlenaNik <alena.nik.digital@gmail.com>
2020-04-09 14:21:21 +0200

    sample json

--- src/components/UpdateInfo.vue
+++ src/components/UpdateInfo.vue
@@ -4,6 +4,10 @@
       edit & add address.
       {{ this.$route.params.id }}
     </h2>
+    <div class="test-json">
+        <h2> sample json </h2>
+        {{ userinfo }}
+    </div>
   </div>
 </template>
 
@@ -13,20 +17,37 @@ export default {
   name: 'UpdateInfo',
   data() {
     return {
-
+        userinfo: []
     }
   },
   created() {
-    let ref;
-    axios.get('http://localhost:3001/api/transactions')
-      .then(res =>
-        ref = res.data)
-
+    let ref = this.$route.params.id
+    this.getData(ref)
+  },
+  methods: {
+    async getData(id) {
+      let res = await axios.get("http://localhost:3001/api/addresses/" + id);
+      this.userinfo = res.data
+      console.log(this.userinfo)
+    }
   }
 }
 </script>
 
 <style lang="scss">
   @import "../assets/scss/variables";
+  .test-json {
+    min-width: 200px;
+    background-color: $base-grey-color;
+    padding: $padding * 2;
+    font-weight: $bold;
+    font-size: 14px;
+    line-height: 20px;
+    margin-bottom: 10px;
+    margin-top: 0;
+    text-align: left;
+
+
+  }
 
 </style>
