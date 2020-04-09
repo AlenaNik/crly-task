e18af6347c467ef84771d2438cb4730622aa2ef7
AlenaNik <alena.nik.digital@gmail.com>
2020-04-09 14:41:42 +0200

    form with data

--- src/components/UpdateInfo.vue
+++ src/components/UpdateInfo.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="edit-address-container">
+  <div v-if="userinfo" class="edit-address-container">
     <h2>
-      edit & add address.
-      {{ this.$route.params.id }}
+      edit address info for {{ userinfo.firstName }} {{ userinfo.lastName }}
     </h2>
+    <form @submit.prevent="EditAddress">
+        <div>
+          <label for=""> {{ userinfo.countryName }}</label>
+          <input type="text" name="country" v-model="countryName">
+        </div>
+      <div>
+        <label for=""> locality: {{ userinfo.locality }} </label>
+        <input type="text" name="country" v-model="country">
+      </div>
+      <div>
+        <label for=""> postal code: {{ userinfo.postalCode }} </label>
+        <input type="text" name="country" v-model="country">
+      </div>
+      <div>
+        <label for=""> street: {{ userinfo.streetAddress }} </label>
+        <input type="text" name="country" v-model="country">
+      </div>
+      <button> Update Info </button>
+    </form>
     <div class="test-json">
-        <h2> sample json </h2>
+        <h2> sample json of the user  </h2>
         {{ userinfo }}
     </div>
+  </div>
+  <div v-else>
+      <h2> someting probably went wrong 🐛 </h2>
   </div>
 </template>
 
@@ -17,7 +38,7 @@ export default {
   name: 'UpdateInfo',
   data() {
     return {
-        userinfo: []
+        userinfo: null
     }
   },
   created() {
@@ -28,7 +49,6 @@ export default {
     async getData(id) {
       let res = await axios.get("http://localhost:3001/api/addresses/" + id);
       this.userinfo = res.data
-      console.log(this.userinfo)
     }
   }
 }
@@ -36,6 +56,9 @@ export default {
 
 <style lang="scss">
   @import "../assets/scss/variables";
+  .edit-address-container {
+    text-align: center;
+  }
   .test-json {
     min-width: 200px;
     background-color: $base-grey-color;
